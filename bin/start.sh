95686fea4af90090eb83bd3b72daa73726c38c4b
axrorbek06 <axrorbekyuldashboev@gmail.com>
2026-03-12 17:14:11 +0500

    Reduce Superset startup concurrency

--- bin/start.sh
+++ bin/start.sh
@@ -3,7 +3,7 @@ set -eu
 
 exec /app/.venv/bin/gunicorn \
   --bind "0.0.0.0:${SUPERSET_PORT:-8088}" \
-  --workers 2 \
+  --workers "${GUNICORN_WORKERS:-1}" \
   --threads 8 \
   --timeout 120 \
   "superset.app:create_app()"
